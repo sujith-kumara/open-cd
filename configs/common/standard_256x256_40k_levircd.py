b62d08d6c9a21c256fe888801629cf813a349cf6
sujithkumara <55655629+sujith-kumara@users.noreply.github.com>
2024-01-13 18:54:54 -0800

    Update standard_256x256_40k_levircd.py
    checkpointing updated

--- configs/common/standard_256x256_40k_levircd.py
+++ configs/common/standard_256x256_40k_levircd.py
@@ -109,14 +109,14 @@ param_scheduler = [
     )
 ]
 # training schedule for 40k
-train_cfg = dict(type='IterBasedTrainLoop', max_iters=4000, val_interval=4000)
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=4000, val_interval=2000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=4000,
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=2000,
                     save_best='mIoU'),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='CDVisualizationHook', interval=1, 
